dab81e7885b42cc83a4e2c16a756e9ae2377a58e
Jamnic98 <jamiestimpson30@gmail.com>
2024-10-31 19:37:04 +0000

    update board with individual methods for adding cards

--- app/__test__/test_board.py
+++ app/__test__/test_board.py
@@ -2,24 +2,36 @@ from app.board import Board
 from app.card import Card
 
 
-def test_board():
+test_cards = [Card('A3'), Card('10H'), Card('3S')]
+
+def test_init_board():
     board = Board()
-    # test flop
-    flop_cards = [Card('A3'), Card('10H'), Card('3S')]
-    board.cards.extend(flop_cards)
-    assert len(board.cards) == 3
-    # test turn
-    turn_card = [Card('4H')]
-    board.cards.extend(turn_card)
-    assert len(board.cards) == 4
-    # test river
-    river_card = [Card('2C')]
-    board.cards.extend(river_card)
-    assert len(board.cards) == 5
+    assert len(board.cards) == 0
+    board = Board(test_cards)
+    assert len(board.cards) == len(test_cards)
+    assert board.cards == test_cards
 
 def test_clear_board():
     board = Board()
-    board.cards.extend([Card('A2'), Card('A3'), Card('A4')])
-    assert len(board.cards) == 3
+    board.cards.extend(test_cards)
+    assert len(board.cards) == len(test_cards)
     board.reset()
     assert len(board.cards) == 0
+
+def test_adding_cards():
+    board = Board()
+    # test flop
+    flop_cards = test_cards
+    board.add_flop_cards(flop_cards)
+    assert len(board.cards) == 3
+    assert board.cards == flop_cards
+    # test turn
+    turn_card = Card('4H')
+    board.add_turn_card(turn_card)
+    assert len(board.cards) == 4
+    assert board.cards[3] == turn_card
+    # test river
+    river_card = Card('2C')
+    board.add_river_card(river_card)
+    assert len(board.cards) == 5
+    assert board.cards[4] == river_card
--- app/board.py
+++ app/board.py
@@ -3,14 +3,36 @@ from app.card import Card
 
 
 class Board:
-    def __init__(self):
-        self.cards = []
+    def __init__(self, cards: List[Card]=None):
+        self.cards = cards or []
 
     def __repr__(self):
         return str(self.cards)
 
-    def add_cards(self, cards: List[Card]):
-        self.cards.extend(cards)
+    def add_flop_cards(self, flop_cards: List[Card]):
+        try:
+            if len(flop_cards) == 3:
+                self.reset()
+                self.cards.extend(flop_cards)
+            raise ValueError
+        except ValueError as e:
+            print(e)
+
+    def add_turn_card(self, turn_card: Card):
+        try:
+            if len(self.cards) == 3:
+                self.cards.append(turn_card)
+            raise ValueError
+        except ValueError as e:
+            print(e)
+
+    def add_river_card(self, river_card: Card):
+        try:
+            if len(self.cards) == 4:
+                self.cards.append(river_card)
+            raise ValueError
+        except ValueError as e:
+            print(e)
 
     def reset(self):
         self.cards.clear()
