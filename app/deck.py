f5cbf23bbe3e50644c6c48336afbd338980cadef
Jamnic98 <jamie@qudo.ai>
2024-11-03 23:04:08 +0000

    remove corresponding methods from deck

--- app/deck.py
+++ app/deck.py
@@ -16,34 +16,6 @@ class Deck:
                 cards.append(Card(value + suit))
         return cards
 
-    def get_royal_flush(self) -> List[Card]:
-        return self.cards
-
-    def get_straight_flush(self) -> List[Card]:
-        return self.cards
-
-    def get_four_of_a_kind(self) -> List[Card]:
-        return self.cards
-
-    def get_full_house(self) -> List[Card]:
-        return self.cards
-
-    def get_flush(self) -> List[Card]:
-        return self.cards
-
-    def get_straight(self) -> List[Card]:
-        return self.cards
-
-    def get_three_of_a_kind(self) -> List[Card]:
-        return self.cards
-
-    def get_two_pair(self) -> List[Card]:
-        return self.cards
-
-    def get_pair(self) -> List[Card]:
-        random_face = choice(FACES)
-        return self.cards
-
     def shuffle(self) -> None:
         shuffle(self.cards)
 
