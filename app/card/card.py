d762f7cb5e31ae70a688be1055cb0e46585ab608
Jamnic98 <jamiestimpson30@gmail.com>
2024-11-08 16:44:04 +0000

    save

---
+++ app/card/card.py
@@ -0,0 +1,15 @@
+from app.utils.constants import CARD_FACE_VALUE_MAP, CARD_FACE_NAME_MAP, CARD_SUIT_NAME_MAP, CARD_SUIT_ICON_MAP
+
+
+class Card:
+    def __init__(self, token: str):
+        self.face: str = token[:-1]
+        self.suit: str = token[-1]
+        self.name: str = self.__set_name()
+        self.face_value: int = CARD_FACE_VALUE_MAP.get(self.face)
+
+    def __repr__(self):
+        return str(self.face + CARD_SUIT_ICON_MAP.get(self.suit))
+
+    def __set_name(self):
+        return f'{CARD_FACE_NAME_MAP.get(self.face)} of {CARD_SUIT_NAME_MAP.get(self.suit)}'
---
+++ app/card/card_holder.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+from .card import Card
+
+
+class CardHolder:
+    def __init__(self, cards: Optional[List[Card]]):
+        self.cards = cards or []
+    
+    def __repr__(self):
+        return str(self.cards)
+
+    def __len__(self):
+        return len(self.cards)
+
+    def get_cards(self):
+        return self.cards
+
+    def add_cards(self, cards: List[Card]):
+        self.cards.extend(cards)
+
+    def reset(self):
+        self.cards.clear()
